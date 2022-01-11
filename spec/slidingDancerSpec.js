5538fcaf8e7c9306c71184c949392002bc6c24fd
Inny Choi <innychoi93@gmail.com>
2022-01-10 21:49:56 -0600

    created a new factory of sliding dancers

---
+++ spec/slidingDancerSpec.js
@@ -0,0 +1,36 @@
+describe('slidingDancer', function() {
+
+  var slidingDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    slidingDancer = new makeSlidingDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(slidingDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  xit('should have a step function that makes its node slide', function() {
+    sinon.spy(slidingDancer.$node, 'toggle');
+    slidingDancer.step();
+    expect(slidingDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(slidingDancer, 'step');
+      expect(slidingDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(slidingDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(slidingDancer.step.callCount).to.be.equal(2);
+    });
+  });
+
+
+});
