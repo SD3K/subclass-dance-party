30a71b5eac769f5451e0cd2d92c108ffe6510596
Inny Choi <innychoi93@gmail.com>
2022-01-11 19:13:47 -0600

    added mouseover event

--- src/slidingDancer.js
+++ src/slidingDancer.js
@@ -1,20 +1,39 @@
-var makeSlidingDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.apply(this, arguments);
+var SlidingDancer = function(top, left, timeBetweenSteps) {
+  Dancer.apply(this, arguments);
 };
 
-makeSlidingDancer.prototype = Object.create(makeDancer.prototype);
-makeSlidingDancer.prototype.constructor = makeSlidingDancer;
+SlidingDancer.prototype = Object.create(Dancer.prototype);
+SlidingDancer.prototype.constructor = SlidingDancer;
 
-makeSlidingDancer.prototype.step = function() {
-  makeDancer.prototype.step.call(this);
-  let max = 10;
-  let min = -10;
-  this.top += Math.floor(Math.random() * (max - min + 1)) + min;
-  this.left += Math.floor(Math.random() * (max - min + 1)) + min;
-  var styleSettings = {
-    top: this.top,
-    left: this.left
-  };
-  this.$node.css(styleSettings);
+SlidingDancer.prototype.step = function() {
+  if (!window.aligned) {
+    Dancer.prototype.step.call(this);
+    let max = 10;
+    let min = -10;
+    this.top += Math.floor(Math.random() * (max - min + 1)) + min;
+    // keep the dancer on the screen
+    while (this.top < 90 || this.top > window.innerHeight - 90) {
+      this.top += Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    this.left += Math.floor(Math.random() * (max - min + 1)) + min;
+    // keep the dancer on the screen
+    while (this.left < 90 || this.left > window.innerWidth - 90) {
+      this.left += Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    var styleSettings = {
+      top: this.top,
+      left: this.left
+    };
+    this.$node.animate(styleSettings);
+  }
+
+  this.$node.mouseenter(function(event) {
+    $(this).css('transform', 'rotate(270deg)');
+    // $(this).animate({'-moz-transform': 'rotate(90deg)'}, 350);
+  });
+
+  this.$node.mouseleave(function(event) {
+    $(this).css('transform', 'rotate(0deg)');
+  });
 };
 
