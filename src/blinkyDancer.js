0b424223b3c0109fe75f1671ae1e8504b3b02c74
Inny Choi <innychoi93@gmail.com>
2022-01-11 19:11:56 -0600

    added mouseover event

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,9 +1,5 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.apply(this, arguments);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  // this.oldStep = makeDancer.prototype.step.bind(this);
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
