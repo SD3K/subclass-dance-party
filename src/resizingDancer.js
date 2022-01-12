4b17550a2718f86079e101517e996bfb0af061da
Inny Choi <innychoi93@gmail.com>
2022-01-11 19:12:20 -0600

    added mouseover event and animation

--- src/resizingDancer.js
+++ src/resizingDancer.js
@@ -1,17 +1,29 @@
-var makeResizingDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.apply(this, arguments);
+var ResizingDancer = function(top, left, timeBetweenSteps) {
+  Dancer.apply(this, arguments);
   this.resize = 0;
 };
 
-makeResizingDancer.prototype = Object.create(makeDancer.prototype);
-makeResizingDancer.prototype.constructor = makeResizingDancer;
+ResizingDancer.prototype = Object.create(Dancer.prototype);
+ResizingDancer.prototype.constructor = ResizingDancer;
 
-makeResizingDancer.prototype.step = function() {
+ResizingDancer.prototype.step = function() {
   // this.$node.css('border-radius', '10px');
-  let max = 90;
-  let min = 20;
-  this.resize = Math.floor(Math.random() * (max - min + 1)) + min;
-  makeDancer.prototype.step.call(this);
-  this.$node.css('width', this.resize + 'px');
-  this.$node.css('height', this.resize + 'px');
+  // let max = 90;
+  // let min = 20;
+  // this.resize = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Dancer.prototype.step.call(this);
+  // this.$node.css('width', this.resize + 'px');
+  // this.$node.css('height', this.resize + 'px');
+  Dancer.prototype.step.call(this);
+  this.$node.animate({width: 'toggle'}, 350);
+
+  this.$node.mouseenter(function(event) {
+    $(this).css('transform', 'rotate(270deg)');
+    // $(this).animate({'-moz-transform': 'rotate(90deg)'}, 350);
+  });
+
+  this.$node.mouseleave(function(event) {
+    $(this).css('transform', 'rotate(0deg)');
+  });
+
 };
